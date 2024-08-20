6f57d3bb9f3d3467f53c64813e5d4efe8b01410c
Banda-Aakash <aakashbanda03@gmail.com>
2024-08-20 19:58:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import About from './Components/About';
+import Book from './Components/Book';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='example' style={styles.appContainer}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/book" element={<Book />} />
+        </Routes>
+        <hr></hr>
+        <Footer/>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const styles = {
+  appContainer: {
+    backgroundColor: 'black',
+    minHeight: '100vh', 
+    color: 'white',
+    textAlign: 'center',
+  },
+};
+
